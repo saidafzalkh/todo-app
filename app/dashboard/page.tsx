e08fbc872a5221ce64a7a1e1724e17e52dd46f5c
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-05-04 22:47:50 +0500

    debugging on host

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,9 +8,7 @@ export default async function Page() {
 
   return (
     <main className="p-2 sm:p-4">
-      <pre>
-        {JSON.stringify(session, null, 2)}
-      </pre>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
       <Dashboard user={session.user} />
     </main>
   );
