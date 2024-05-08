14a30200892e47d7513f7590716bc919c5afbc46
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-05-09 01:22:59 +0500

    removed border on loading

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,10 +1,9 @@
-import { Card } from "./ui/card";
 import { ScaleLoader } from "react-spinners";
 
 export default function Loading() {
   return (
-    <Card className="h-[250px] flex items-center justify-center">
+    <div className="h-[350px] flex items-center justify-center">
       <ScaleLoader color="#00B0FF" />
-    </Card>
+    </div>
   );
 }
